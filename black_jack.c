c1170cd2591b9409c8b3861407d6e0e80a82035d
oninz <oninesguerra7@gmail.com>
2026-05-11 21:15:26 +0800

    Initialize Blackjack game structure
    Added function prototypes and main function structure.

--- black_jack.c
+++ black_jack.c
@@ -2,3 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+//function prototypes
+int check_win();
+int bet();
+int dealer_logic();
+void deal_cards();
+
+int main{
+  int dealer = 0;
+  int user = 0;
+  int money = 1000;
+  int bet;
+  int choice;
+
+  printf("Welcome to Blackjack\n");
+  printf("=====BLACKJACK===== \n");
+
+  while (money >0){
+    
+    
+  }
+}
