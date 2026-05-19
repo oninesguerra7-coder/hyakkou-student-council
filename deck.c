5f899317b52550810a17dc5ab01640ba242714de
syxtema <164163447+syxtema@users.noreply.github.com>
2026-05-19 23:57:07 +0800

    Create deck.c

---
+++ deck.c
@@ -0,0 +1,20 @@
+void shuffleDeck(Game *g) {
+    // Start from last card and swap backwards
+    for (int i = DECK_SIZE - 1; i > 0; i--) {
+        
+        int j = rand() % (i + 1); // random index
+        // swap cards
+        Card temp = g->deck[i];
+        g->deck[i] = g->deck[j];
+        g->deck[j] = temp;
+    }
+    g->deckIndex = 0;
+}
+
+Card drawCard(Game *g) { // to draw one card
+
+    if (g->deckIndex >= DECK_SIZE)
+        shuffleDeck(g); // reshuffle deck
+
+    return g->deck[g->deckIndex++];
+}
