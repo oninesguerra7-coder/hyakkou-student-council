a88c1f80bf24950f65d72e5e2d868b8c211a97d0
syxtema <164163447+syxtema@users.noreply.github.com>
2026-05-19 23:48:01 +0800

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_CARDS      15
+#define DECK_SIZE      52
+#define BLACKJACK      21
+#define DEALER_STAND   17
+#define STARTING_MONEY 1000
+
+// ANSI colors
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define CYN   "\x1B[36m"
+#define MGT   "\x1B[35m"
+#define RESET "\x1B[0m"
+
+#endif
